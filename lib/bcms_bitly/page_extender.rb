bd9f5ec4fc386c58589deaf688a83b2a56b7a378
Josiah Ivey <josiah.ivey@gmail.com>
2012-09-24 09:25:45 -0500

    update short link url if page path is changed

--- lib/bcms_bitly/page_extender.rb
+++ lib/bcms_bitly/page_extender.rb
@@ -1,15 +1,16 @@
 module BcmsBitly
   module PageExtender
     def self.included(model)
-      model.after_create :bcms_bitly_create_short_link
+      model.before_save :bcms_bitly_create_short_link
       model.has_one :short_link, class_name: 'BcmsBitly::ShortLink'
 
       def bcms_bitly_create_short_link
+        return unless path.present? && path_changed?
         domain  = Rails.configuration.cms.site_domain
-        sl      = build_short_link
+        sl      = short_link || build_short_link
         sl.url  = sl.shorten("http://#{domain}#{path}")
         sl.name = name
-        sl.save
+        self.short_link = sl
       end
     end
   end
--- test/unit/bcms_bitly/short_link_test.rb
+++ test/unit/bcms_bitly/short_link_test.rb
@@ -2,17 +2,33 @@ require 'test_helper'
 
 module BcmsBitly
   class ShortLinkTest < ActiveSupport::TestCase
-    test "shorten returns a short URL from bitly" do
-      ShortLink.new.shorten('http://google.com')
+    test 'shorten returns a short URL from bitly' do
+      ShortLink.new.shorten 'http://google.com'
     end
 
-    test "a created page causes a ShortLink to be created" do
+    test 'a created page causes a ShortLink to be created' do
       assert_difference ['Cms::Page.count', 'BcmsBitly::ShortLink.count'] do
-        @page = Cms::Page.create(name: 'Test', path: 'test')
+        @page = Cms::Page.create name: 'Test', path: '/test'
       end
 
       assert_present @page.short_link
       assert_equal @page.name, @page.short_link.name
+    end
+
+    test 'an existing page updates its ShortLink when the path is changed' do
+      page = Cms::Page.create name: 'Test', path: '/test'
+      old_url = page.short_link.url
+      page.update_attribute :path, '/test2'
+
+      assert page.short_link.url != old_url
+    end
+
+    test 'ShortLink is not updated when page is saved but path is unchanged' do
+      page = Cms::Page.create name: 'Test', path: '/test'
+      old_url = page.short_link.url
+      page.update_attribute :name, 'Test2'
+
+      assert_equal page.short_link.url, old_url
     end
   end
 end
